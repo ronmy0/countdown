eed5b654ae83d761073598aab8fc21d26286d9fd
ronmy0 <gabriels.freitas2401@gmail.com>
2024-09-29 18:11:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function playAlertSound() {
 }
 
 // Specify the exact time when the sound should play
-const targetDays = 39;
+const targetDays = 36;
 const targetHours = 22;
-const targetMinutes = 3;
+const targetMinutes = 30;
 const targetSeconds = 40;
 
 // Function to display the countdown to midnight UTC on November 6th, 2024
